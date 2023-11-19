58372fc6a057f3710b6655ae70ba7c82ea29e82a
woomae <cwg0809@naver.com>
2023-11-19 21:06:13 +0900

    feature: healthcheck api추가
    - healthcheck api추가

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from dotenv import load_dotenv
 from assistant import callchat
@@ -17,6 +17,10 @@ def chatbot():
             result = callchat(user_message)
             return result
     return "message not found", 400
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify(result="ApiMessages.OK")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=1234) #서비스시 host값 ip로 변경, port도 변경, route경로도 확인할것
